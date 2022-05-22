c2051a7c1ed5ac9ec526bb691370b41c97f36abd
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2022-05-22 16:58:22 +0430

    Setting blog admin and models for admin panel

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('pub', 'Published'),
+        ('drf', 'Draft'),
+    )
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    status = models.CharField(choices=STATUS_CHOICES, max_length=3)
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+

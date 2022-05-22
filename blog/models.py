49b221d44cfb0f1d59917169b284dcc3447f9101
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2022-05-22 17:01:19 +0430

    show title Posts in Data-Base

--- blog/models.py
+++ blog/models.py
@@ -13,3 +13,5 @@ class Post(models.Model):
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.title

7026e1eb2a8f8f405f9bc3bce238b242d7e84388
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2022-05-22 22:59:37 +0430

    upgrade blog/admin.py to beter show in admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'datetime_created', 'status')
+
+
+# admin.site.register(Post, PostAdmin)

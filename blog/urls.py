18b5bbbcf5bc51007bc2a19087e8b4257d1c467b
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2022-05-23 20:04:27 +0430

    Setting App blog for entire posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,7 @@
-from idlelib.textview import view_file
-
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.post_list_view, name='post list view')
 ]
+
--- blog/views.py
+++ blog/views.py
@@ -5,3 +5,4 @@ from .models import Post
 def post_list_view(request):
     entire_posts = Post.objects.all()
     return render(request, 'blog/posts_list.html', {'posts_list': entire_posts})
+

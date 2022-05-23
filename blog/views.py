6adf5301a32054534c52bf306a017db00a4d1335
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2022-05-23 05:59:47 +0430

    add view and html

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+def post_list_view(request):
+    entire_posts = Post.objects.all()
+    return render(request, 'blog/posts.html', {'posts_list': entire_posts})

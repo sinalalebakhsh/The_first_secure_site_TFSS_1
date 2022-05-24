ec4bf86bb78549e522ceef5c6020ae69c9026950
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2022-05-24 23:38:01 +0430

    handle wrong search in id posts

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Post
 from django.http import HttpResponse
-
+from django.core.exceptions import ObjectDoesNotExist
 
 def post_list_view(request):
     entire_posts = Post.objects.all()
@@ -9,6 +9,10 @@ def post_list_view(request):
 
 
 def post_detail_view(request, pk):
-    post = Post.objects.get(pk=pk)
+    try:
+        post = Post.objects.get(pk=pk)
+    except ObjectDoesNotExist:
+        post = None
+        print('Exception ObjectDoesNotExist')
     return render(request, 'blog/post_detail.html', {'post': post})
 

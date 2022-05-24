d090bf3230c2893be05ebbba53a184e912128d0b
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2022-05-24 11:50:58 +0430

    get number default from user with <int:pk> in blog/urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list_view, name='post list view')
+    path('', views.post_list_view, name='post list view'),
+    path('<int:pk>/', views.post_detail_view, name='post detail'),
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from .models import Post
+from django.http import HttpResponse
 
 
 def post_list_view(request):
     entire_posts = Post.objects.all()
     return render(request, 'blog/posts_list.html', {'posts_list': entire_posts})
+
+
+def post_detail_view(request, pk):
+    print('id in url: ', pk)
+    return HttpResponse(f'id: {pk}')
 

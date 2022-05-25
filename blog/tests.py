2f82762ae7db9aed585bd7de84893a5c43b07ce1
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2022-05-25 18:13:16 +0430

    add class BlogPostTest in Tests.py

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Post
+
+
+class BlogPostTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='user1')
+        self.post1 = Post.objects.create(
+            title='Post1',
+            text='This is the description of Post1',
+            status=Post.STATUS_CHOICES[0],
+            author=self.user,
+        )

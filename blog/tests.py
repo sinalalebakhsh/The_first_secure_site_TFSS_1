2992a1f203d2144a374ea74bd1459ba9892408aa
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2022-05-30 00:49:52 +0430

    this is debuged Fu.ck.ing!!!! tests its so nervouse :(((((

--- blog/tests.py
+++ blog/tests.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import User
 from django.test import TestCase
 from .models import Post
 from django.contrib.auth.models import User
+from django.shortcuts import reverse
+
 
 class PostTest(TestCase):
     def setUp(self):
@@ -12,8 +14,50 @@ class PostTest(TestCase):
             status=Post.STATUS_CHOICES[0][0],
             author=self.user,
         )
+        self.post2 = Post.objects.create(
+            title='Post2',
+            text='This is for Post2',
+            status=Post.STATUS_CHOICES[1][0],
+            author=self.user,
+        )
+
+
 
     def test_post_list_url(self):
         response = self.client.get('/blog/')
         self.assertEqual(response.status_code, 200)
 
+
+################################################################################
+
+
+
+    def test_post_list_url(self):
+        response = self.client.get('/blog/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_list_url_by_name(self):
+        response = self.client.get(reverse('post list view'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_title_on_blog_list_page(self):
+        response = self.client.get(reverse('post list view'))
+        self.assertContains(response, self.post1.title)
+
+    def test_post_detail_url(self):
+        response = self.client.get(reverse('post detail', args=[self.post1.id]))
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_details_on_blog_detail_page(self):
+        response = self.client.get(reverse('post detail', args=[self.post1.id]))
+        self.assertContains(response, self.post1.title)
+        self.assertContains(response, self.post1.text)
+
+    def test_status_404_if_post_id_not_exist(self):
+        response = self.client.get(reverse('post detail', args=[999]))
+        self.assertEqual(response.status_code, 404)
+
+    def test_draft_post_not_show_in_posts_list(self):
+        response = self.client.get(reverse('post list view'))
+        self.assertContains(response, self.post1.title)
+        self.assertNotContains(response, self.post2.title)

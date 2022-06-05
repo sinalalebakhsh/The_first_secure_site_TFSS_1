5f18678ba059d82d375f74e61c677f2c3d6e5684
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2022-06-05 22:45:19 +0430

    Every part in program is important: for this time i set the title in bar of browser is little push but is important

--- blog/views.py
+++ blog/views.py
@@ -13,11 +13,11 @@ def post_list_view(request):
 
 def post_detail_view(request, pk):
     post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
     # is another way to setting wrong link
     # try:
     #     post = Post.objects.get(pk=pk)
     # except ObjectDoesNotExist:
     #     post = None
     #     print('Exception ObjectDoesNotExist')
-    return render(request, 'blog/post_detail.html', {'post': post})
 

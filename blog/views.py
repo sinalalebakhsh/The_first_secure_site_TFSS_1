da506032be61e20d379f4d2c360339826299ffcd
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2022-06-20 22:36:29 +0430

    add first query set for just Published post not for draft posts

--- blog/views.py
+++ blog/views.py
@@ -18,6 +18,9 @@ class PostListView(generic.ListView):
     template_name = 'blog/posts_list.html'
     context_object_name = 'posts_list'
 
+    def get_queryset(self):
+        return Post.objects.filter(status='pub').order_by('-datetime_modified')
+
 
 
 def post_detail_view(request, pk):
